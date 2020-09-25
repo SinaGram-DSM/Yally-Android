496a14ad0a03cb9e5d809dac59e60ca5746c6b26
sirasatarato <sirasatarato@gmail.com>
2020-09-25 20:03:24 +0900

    Make Retrofit in YallyConnector

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Network/YallyConnector.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Network/YallyConnector.kt
@@ -1,5 +1,31 @@
 package com.sinagram.yallyandroid.Network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object YallyConnector {
-    
+    private var retrofit: Retrofit
+    private var api: YallyAPI
+    private const val baseURL = ""
+
+    init {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(baseURL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        api = retrofit.create(YallyAPI::class.java)
+    }
+
+    fun createAPI() = api
 }
