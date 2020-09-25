2f00885ea5d64aaa7a098cc94760c854325048e4
sirasatarato <sirasatarato@gmail.com>
2020-09-25 19:20:33 +0900

    Create Two Activity
    SignActivity 와 SplashActivity 를 생성함

---
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/SignActivity.kt
@@ -0,0 +1,12 @@
+package com.sinagram.yallyandroid.Sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sinagram.yallyandroid.R
+
+class SignActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign)
+    }
+}
--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
@@ -1,9 +1,10 @@
-package com.sinagram.yallyandroid
+package com.sinagram.yallyandroid.Splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sinagram.yallyandroid.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
