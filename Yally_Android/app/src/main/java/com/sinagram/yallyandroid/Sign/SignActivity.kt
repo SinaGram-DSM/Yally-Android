0490f6f76a12d3b9b941a3bf870ed0b35591abb2
sirasatarato <sirasatarato@gmail.com>
2020-09-29 21:10:11 +0900

    Apply Backward to navigation Button

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/SignActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/SignActivity.kt
@@ -2,11 +2,16 @@ package com.sinagram.yallyandroid.Sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.sinagram.yallyandroid.R
 
 class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+    }
+
+    fun backPress(view: View) {
+        onBackPressed()
     }
 }
