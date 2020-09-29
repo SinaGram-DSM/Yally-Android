abee591eb01b1d51d8fef27f8418c2242bb348ec
sirasatarato <sirasatarato@gmail.com>
2020-09-29 21:05:51 +0900

    Create SignUpFragment

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/LoginFragment.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/LoginFragment.kt
@@ -9,7 +9,8 @@ import com.sinagram.yallyandroid.R
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.signinup_layout, container, false)
---
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/SignUpFragment.kt
@@ -0,0 +1,32 @@
+package com.sinagram.yallyandroid.Sign.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.sinagram.yallyandroid.R
+import kotlinx.android.synthetic.main.signinup_layout.view.*
+
+class SignUpFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.signinup_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
+            signinup_title_textView.text = getString(R.string.welcome_first_visit)
+            signinup_subtitle_textView.text = getString(R.string.start_sign_up)
+            signinup_name_editText.visibility = View.VISIBLE
+            signinup_do_sign_button.text = getString(R.string.sign_in)
+            signinup_forgot_textView.visibility = View.GONE
+        }
+    }
+}
