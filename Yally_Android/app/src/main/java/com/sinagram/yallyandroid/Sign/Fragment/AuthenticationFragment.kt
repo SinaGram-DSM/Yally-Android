505682559cf399effdde758347675bea57fa7004
sirasatarato <sirasatarato@gmail.com>
2020-09-29 21:59:20 +0900

    Create AuthenticationFragment

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/AuthenticationFragment.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/AuthenticationFragment.kt
@@ -1,26 +1,18 @@
 package com.sinagram.yallyandroid.Sign.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.sinagram.yallyandroid.R
 
-class LoginFragment : Fragment() {
+class AuthenticationFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.signinup_layout, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        requireActivity().findViewById<TextView>(R.id.sign_title_textView).text =
-            getString(R.string.login)
     }
 }
--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/SignUpFragment.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/SignUpFragment.kt
@@ -1,12 +1,11 @@
 package com.sinagram.yallyandroid.Sign.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.sinagram.yallyandroid.R
 import kotlinx.android.synthetic.main.signinup_layout.view.*
 
