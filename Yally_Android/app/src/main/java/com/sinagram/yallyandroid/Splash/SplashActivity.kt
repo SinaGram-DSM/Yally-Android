4767de0432ff41022d5e978d07a808031c2b7fd1
minhee <jwmh15@gmail.com>
2020-10-02 23:15:50 +0900

    Add splash Xml

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.sinagram.yallyandroid.Splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinagram.yallyandroid.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        splash_login_button.setOnClickListener {
+            val intent = Intent(this,)
+        }
+
+        splash_signUp_button.setOnClickListener {
+            val intent = Intent(this,)
+        }
     }
 }
