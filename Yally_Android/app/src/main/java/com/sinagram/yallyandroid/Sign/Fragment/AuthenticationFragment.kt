e676c7f3959d9d2b4de26e760bbbedce57617266
sirasatarato <sirasatarato@gmail.com>
2020-10-05 14:45:19 +0900

    Make to display Email page

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/AuthenticationFragment.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/AuthenticationFragment.kt
@@ -1,6 +1,8 @@
 package com.sinagram.yallyandroid.Sign.Fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +15,8 @@ import com.sinagram.yallyandroid.Sign.Data.SignProcess
 import com.sinagram.yallyandroid.Sign.SignActivity
 import com.sinagram.yallyandroid.Sign.ViewModel.SignUpViewModel
 import kotlinx.android.synthetic.main.fragment_authentication.view.*
+import kotlinx.android.synthetic.main.signinup_layout.*
+import kotlinx.android.synthetic.main.signinup_layout.view.*
 
 class AuthenticationFragment : Fragment() {
     private val signUpViewModel: SignUpViewModel by viewModels()
@@ -24,5 +28,21 @@ class AuthenticationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.signinup_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
+            changeEmailPage()
+        }
+    }
+
+    private fun changeEmailPage() {
+        signinup_title_textView.text = getString(R.string.welcome_first_visit)
+        signinup_subtitle_textView.text = getString(R.string.start_sign_up)
+        signinup_password_inputLayout.visibility = View.GONE
+        signinup_doSign_button.text = getString(R.string.next)
+        signinup_forgot_textView.visibility = View.GONE
     }
 }
