f48ac5fd7d57a75b3d161b3a195a34c434b6840b
sirasatarato <sirasatarato@gmail.com>
2020-10-05 14:56:44 +0900

    Make createTextWatcher and activeButton

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/AuthenticationFragment.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/AuthenticationFragment.kt
@@ -7,10 +7,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.textfield.TextInputLayout
 import com.sinagram.yallyandroid.R
 import com.sinagram.yallyandroid.Sign.Data.SignProcess
 import com.sinagram.yallyandroid.Sign.SignActivity
@@ -18,10 +20,14 @@ import com.sinagram.yallyandroid.Sign.ViewModel.SignUpViewModel
 import kotlinx.android.synthetic.main.fragment_authentication.view.*
 import kotlinx.android.synthetic.main.signinup_layout.*
 import kotlinx.android.synthetic.main.signinup_layout.view.*
+import java.util.regex.Pattern
 
 class AuthenticationFragment : Fragment() {
     private val signUpViewModel: SignUpViewModel by viewModels()
     private val hashMap: HashMap<String, String> = HashMap()
+    private val emailPattern =
+        Pattern.compile("^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\\.([a-zA-Z])+([a-zA-Z])+")
+    private var email = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +42,9 @@ class AuthenticationFragment : Fragment() {
 
         view.apply {
             changeEmailPage()
+            signinup_email_editText.addTextChangedListener(
+                createTextWatcher(signinup_email_inputLayout)
+            )
         }
     }
 
@@ -53,5 +62,35 @@ class AuthenticationFragment : Fragment() {
         signinup_password_inputLayout.visibility = View.GONE
         signinup_doSign_button.text = getString(R.string.next)
         signinup_forgot_textView.visibility = View.GONE
+    }
+
+    private fun createTextWatcher(textInputLayout: TextInputLayout): TextWatcher {
+        return object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textInputLayout.error = null
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                when (textInputLayout) {
+                    signinup_email_inputLayout -> email = p0.toString()
+                }
+                activeButton(signinup_doSign_button)
+            }
+        }
+    }
+
+    private fun activeButton(button: Button) {
+        button.apply {
+            when {
+                email.length <= 30 && emailPattern.matcher(email).matches() -> {
+                    setBackgroundResource(R.drawable.button_gradient)
+                }
+                else -> {
+                    setBackgroundResource(R.drawable.button_bright_gray)
+                    setOnClickListener(null)
+                }
+            }
+        }
     }
 }
