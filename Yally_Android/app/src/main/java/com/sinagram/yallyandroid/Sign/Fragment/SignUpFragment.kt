b8acda62c234c6bec124f8c19cdaf9c34459d447
sirasatarato <sirasatarato@gmail.com>
2020-10-05 17:11:38 +0900

    Clear SignUpFragment code

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/SignUpFragment.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Sign/Fragment/SignUpFragment.kt
@@ -18,7 +18,6 @@ import kotlinx.android.synthetic.main.signinup_layout.view.*
 
 class SignUpFragment : Fragment() {
     private val signUpViewModel: SignUpViewModel by viewModels()
-    private var email = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,55 +25,5 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.signinup_layout, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.apply {
-            signinup_title_textView.text = getString(R.string.welcome_first_visit)
-            signinup_subtitle_textView.text = getString(R.string.start_sign_up)
-            signinup_doSign_button.text = getString(R.string.sign_up)
-            signinup_forgot_textView.visibility = View.GONE
-            signinup_doSign_button.setOnClickListener {
-                email = signinup_email_editText.text.toString()
-                val password = signinup_password_editText.text.toString()
-
-                val signUpRequest = SignUpRequest(email, password, "nickname", 0)
-
-                signUpViewModel.checkSignUpInfo(signUpRequest)
-            }
-        }
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        val signActivity = activity as SignActivity
-
-        signActivity.findViewById<TextView>(R.id.sign_title_textView).text =
-            getString(R.string.sign_up)
-
-        signUpViewModel.errorMessageLiveData.observe(viewLifecycleOwner, {
-            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
-        })
-
-        signUpViewModel.signUpSuccessLiveData.observe(viewLifecycleOwner, {
-            when(it) {
-                SignProcess.Create -> {
-                    moveToAuthentication()
-                }
-                else -> {
-                    Log.e("SignUpFragment", "알 수 없는 오류")
-                }
-            }
-        })
-    }
-
-    private fun moveToAuthentication() {
-        (activity as SignActivity).replaceFragment(AuthenticationFragment().apply {
-            arguments = Bundle().apply {
-                putString("UserEmail", email)
-            }
-        })
     }
 }
