ddb38d33efa77c88c949fd2f2e4b7a6ed7311a41
sirasatarato <sirasatarato@gmail.com>
2020-10-13 19:31:12 +0900

    Fixed Splash design

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.sinagram.yallyandroid.Splash
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.sinagram.yallyandroid.R
 import com.sinagram.yallyandroid.Sign.SignActivity
 import kotlinx.android.synthetic.main.activity_splash.*
@@ -11,6 +13,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setTransparentStatusBar()
 
         splash_login_button.setOnClickListener {
             val intent = Intent(this, SignActivity::class.java)
@@ -23,5 +26,11 @@ class SplashActivity : AppCompatActivity() {
             intent.putExtra("splash", "splash_signUp")
             startActivity(intent)
         }
+    }
+
+    private fun setTransparentStatusBar() {
+        window?.decorView?.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = Color.TRANSPARENT
     }
 }
