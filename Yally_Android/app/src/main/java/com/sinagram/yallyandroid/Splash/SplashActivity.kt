dc45f6f0c341ae68e21824d7c0bbda407a263daa
sirasatarato <sirasatarato@gmail.com>
2020-10-13 19:08:41 +0900

    Apply design and Fixed somthing

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinagram.yallyandroid.R
+import com.sinagram.yallyandroid.Sign.SignActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
@@ -12,14 +13,14 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         splash_login_button.setOnClickListener {
-            val intent = Intent(this,HomeActivity.class)
-            intent.putExtra("splash","splash_login")
+            val intent = Intent(this, SignActivity::class.java)
+            intent.putExtra("splash", "splash_login")
             startActivity(intent)
         }
 
         splash_signUp_button.setOnClickListener {
-            val intent = Intent(this,HomeActivity.class)
-            intent.putExtra("splash","splash_signUp")
+            val intent = Intent(this, SignActivity::class.java)
+            intent.putExtra("splash", "splash_signUp")
             startActivity(intent)
         }
     }
