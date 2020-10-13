90d40cae94947d1bf7c8cd99445b8870b4807897
sirasatarato <sirasatarato@gmail.com>
2020-10-13 20:25:41 +0900

    Apply design on bottom navigation bar

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/HomeActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/HomeActivity.kt
@@ -3,10 +3,13 @@ package com.sinagram.yallyandroid.Home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinagram.yallyandroid.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        home_bottom_navigationView.background = null
+        home_bottom_navigationView.menu.getItem(1).isEnabled = false
     }
 }
