a41a36c010e2fd4da862f818edead58b5ac3c8d9
sirasatarato <sirasatarato@gmail.com>
2020-10-15 20:20:47 +0900

    Change CircleMenu to Floating Action button

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/HomeActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/HomeActivity.kt
@@ -1,15 +1,43 @@
 package com.sinagram.yallyandroid.Home
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.sinagram.yallyandroid.R
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+    private var fab_open: Animation? = null
+    private var fab_close: Animation? = null
+    private var isFabOpen = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         home_bottom_navigationView.background = null
         home_bottom_navigationView.menu.getItem(1).isEnabled = false
+
+        fab_open = AnimationUtils.loadAnimation(this, R.anim.fab_open)
+        fab_close = AnimationUtils.loadAnimation(this, R.anim.fab_close)
+        home_circleMenuButton.setOnClickListener {
+            toggleFab()
+        }
+    }
+
+    private fun toggleFab() {
+        isFabOpen = if (isFabOpen) {
+            home_writing_button.startAnimation(fab_close)
+            home_user_profile_button.startAnimation(fab_close)
+            home_writing_button.isClickable = false
+            home_user_profile_button.isClickable = false
+            false
+        } else {
+            home_writing_button.startAnimation(fab_open)
+            home_user_profile_button.startAnimation(fab_open)
+            home_writing_button.isClickable = true
+            home_user_profile_button.isClickable = true
+            true
+        }
     }
 }
