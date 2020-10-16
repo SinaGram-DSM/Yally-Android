174c1b5434dc5f663250184429d2b6b3a88af05b
sirasatarato <sirasatarato@gmail.com>
2020-10-16 12:46:06 +0900

    Clean up the code on home activity

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/HomeActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/HomeActivity.kt
@@ -9,45 +9,47 @@ import com.sinagram.yallyandroid.R
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
-    private var fab_open: Animation? = null
-    private var fab_close: Animation? = null
+    private var fabOpen: Animation? = null
+    private var fabClose: Animation? = null
     private var isFabOpen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        initialization()
+    }
+
+    private fun initialization() {
         home_bottom_navigationView.background = null
         home_bottom_navigationView.menu.getItem(1).isEnabled = false
 
-        fab_open = AnimationUtils.loadAnimation(this, R.anim.fab_open)
-        fab_close = AnimationUtils.loadAnimation(this, R.anim.fab_close)
-        home_circleMenuButton.setOnClickListener {
-            toggleFab()
-        }
+        fabOpen = AnimationUtils.loadAnimation(this, R.anim.fab_open)
+        fabClose = AnimationUtils.loadAnimation(this, R.anim.fab_close)
     }
 
-    private fun toggleFab() {
-        isFabOpen = if (isFabOpen) {
-            home_writing_button.startAnimation(fab_close)
-            home_user_profile_button.startAnimation(fab_close)
-            home_writing_button.isClickable = false
-            home_user_profile_button.isClickable = false
-            home_circleMenuButton.animate().scaleX(1f).scaleY(1f).setDuration(300).withStartAction {
+    fun toggleFab(view: View) {
+        val animator = home_circleMenuButton.animate()
+
+        if (isFabOpen) {
+            home_writing_button.startAnimation(fabClose)
+            home_user_profile_button.startAnimation(fabClose)
+            animator.scaleX(1f).scaleY(1f).setDuration(300).withStartAction {
                 home_circleMenuButton.scaleX = 1.5f
                 home_circleMenuButton.scaleY = 1.5f
-            }.start()
-            false
+            }
         } else {
-            home_writing_button.startAnimation(fab_open)
-            home_user_profile_button.startAnimation(fab_open)
-            home_writing_button.isClickable = true
-            home_user_profile_button.isClickable = true
-            home_circleMenuButton.animate().scaleX(1.5f).scaleY(1.5f).setDuration(300)
-                .withStartAction {
-                    home_circleMenuButton.scaleX = 1f
-                    home_circleMenuButton.scaleY = 1f
-                }.start()
-            true
+            home_writing_button.startAnimation(fabOpen)
+            home_user_profile_button.startAnimation(fabOpen)
+            animator.scaleX(1.5f).scaleY(1.5f).setDuration(300).withStartAction {
+                home_circleMenuButton.scaleX = 1f
+                home_circleMenuButton.scaleY = 1f
+            }
         }
+
+        animator.start()
+        isFabOpen = !isFabOpen
+        home_writing_button.isClickable = isFabOpen
+        home_user_profile_button.isClickable = isFabOpen
     }
 }
