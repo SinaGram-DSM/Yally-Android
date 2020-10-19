bd73a7fc0ac9108ee7d8c0e324eaad06d4781076
sirasatarato <sirasatarato@gmail.com>
2020-10-19 20:18:18 +0900

    Create SeekBarChangeListenerImpl

---
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Util/SeekBarChangeListenerImpl.kt
@@ -0,0 +1,9 @@
+package com.sinagram.yallyandroid.Util
+
+import android.widget.SeekBar
+
+abstract class SeekBarChangeListenerImpl : SeekBar.OnSeekBarChangeListener {
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {}
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+}
