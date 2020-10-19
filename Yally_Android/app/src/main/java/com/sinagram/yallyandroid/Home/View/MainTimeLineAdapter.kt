406778f4258fc0da07c4b717b3f4a69a3c86a1bd
sirasatarato <sirasatarato@gmail.com>
2020-10-19 13:21:53 +0900

    Add an Action to set itemView

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/View/MainTimeLineAdapter.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/View/MainTimeLineAdapter.kt
@@ -1,13 +1,23 @@
 package com.sinagram.yallyandroid.Home.View
 
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.sinagram.yallyandroid.Home.Data.Post
 import com.sinagram.yallyandroid.R
+import com.sinagram.yallyandroid.Util.FontSpan
+import kotlinx.android.synthetic.main.item_post_cardview.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainTimeLineAdapter(var postsList: List<Post>) :
+class MainTimeLineAdapter(var postsList: List<Post>, val font: Typeface?) :
     RecyclerView.Adapter<MainTimeLineAdapter.MainTimeLineViewHolder>() {
 
     override fun getItemCount(): Int = postsList.size
@@ -19,8 +29,72 @@ class MainTimeLineAdapter(var postsList: List<Post>) :
     }
 
     override fun onBindViewHolder(holder: MainTimeLineViewHolder, position: Int) {
+        val postData = postsList[position]
 
+        holder.apply {
+            itemView.apply {
+                post_user_name_textView.text = postData.user.nickname
+                post_yally_count_textView.text = "얄리 ${postData.yally}개"
+                post_comments_count_textView.text = "댓글 ${postData.comment}개"
+            }
+
+            setTimeFromUploadedTime(postData.createdAt)
+            applyBoldToTags(postData.content)
+            checkClickedYally(postData.isYally)
+        }
     }
 
-    inner class MainTimeLineViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    inner class MainTimeLineViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun setTimeFromUploadedTime(uploadedDate: String) {
+            val now = System.currentTimeMillis()
+            val dateNow = Date(now)
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+            try {
+                val dateCreated = dateFormat.parse(uploadedDate)
+                val duration = dateNow.time - dateCreated.time
+                val min = duration / 60000
+
+                val timeDifference = when {
+                    min < 1 -> "방금전"
+                    min in 1..59 -> "${min}분"
+                    min in 60..1440 -> "${duration / 3600000}시간"
+                    else -> SimpleDateFormat("MM월 dd일").format(dateCreated.time)
+                }
+
+                itemView.post_uploaded_time_textView.text = timeDifference
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        fun checkClickedYally(isYally: Boolean) {
+            if (isYally) {
+                itemView.post_do_yally_imageView.setImageResource(R.drawable.ic_yally_note_colorful)
+                itemView.post_do_yally_textView.setTextColor(Color.parseColor("#6E3CEF"))
+            }
+        }
+
+        fun applyBoldToTags(content: String) {
+            itemView.post_content_textView.text = content
+            var isCharacter = false
+            val span: Spannable = itemView.post_content_textView.text as Spannable
+            var index = 0
+
+            while (content.length >= index + 1) {
+                if (content[index] == '#') isCharacter = !isCharacter
+                if (content[index] == ' ') isCharacter = false
+                if (isCharacter) {
+                    span.setSpan(
+                        FontSpan(font),
+                        index,
+                        index + 1,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                }
+
+                index++
+            }
+        }
+    }
 }
