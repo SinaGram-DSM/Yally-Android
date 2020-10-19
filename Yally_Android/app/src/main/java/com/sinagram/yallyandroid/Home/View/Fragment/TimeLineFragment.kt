c69a278a9b0784dae5043343d7a3aa8a8df3e8d7
sirasatarato <sirasatarato@gmail.com>
2020-10-19 13:22:10 +0900

    Make dummy data

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/View/Fragment/TimeLineFragment.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/View/Fragment/TimeLineFragment.kt
@@ -1,14 +1,20 @@
 package com.sinagram.yallyandroid.Home.View.Fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sinagram.yallyandroid.Home.Data.Post
+import com.sinagram.yallyandroid.Home.Data.PostsResponse
+import com.sinagram.yallyandroid.Home.Data.User
+import com.sinagram.yallyandroid.Home.View.MainTimeLineAdapter
 import com.sinagram.yallyandroid.Home.ViewModel.TimeLineViewModel
 import com.sinagram.yallyandroid.R
+import kotlinx.android.synthetic.main.fragment_time_line.*
 
 class TimeLineFragment : Fragment() {
     private val timeLineViewModel: TimeLineViewModel by viewModels()
@@ -23,10 +29,40 @@ class TimeLineFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        timeLineViewModel.getTimeLineItem(1)
+//        timeLineViewModel.getTimeLineItem(1)
+//
+//        timeLineViewModel.successLiveData.observe(viewLifecycleOwner, {
+//            timeLine_recyclerView.apply {
+//                setHasFixedSize(true)
+//                layoutManager = LinearLayoutManager(activity)
+//                adapter = MainTimeLineAdapter(
+//                    it, ResourcesCompat.getFont(context, R.font.notosanskr_medium)
+//                )
+//            }
+//        })
 
-        timeLineViewModel.successLiveData.observe(viewLifecycleOwner, {
-            Log.e("TimeLineFragment", it.toString())
-        })
+        val post = Post(
+            id = "42736b0ace29e3a9b5cfa3a0240d4c0e",
+            content = "content #c #t",
+            sound = "1602582644569.JPG",
+            img = "1602582644582.JPG",
+            createdAt = "2020-10-13 18:50:44",
+            user = User(
+                email = "admin123@gmail.com",
+                nickname = "admin", img = "user.jpg"
+            ),
+            comment = 0,
+            yally = 0,
+            isYally = true,
+            isMine = true
+        )
+
+        timeLine_recyclerView.apply{
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(activity)
+            adapter = MainTimeLineAdapter(
+                listOf(post), ResourcesCompat.getFont(context, R.font.notosanskr_medium)
+            )
+        }
     }
 }
