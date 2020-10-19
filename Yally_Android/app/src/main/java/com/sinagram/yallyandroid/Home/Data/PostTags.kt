93df2603ce164dfe27c01d411428ce699c48450a
sirasatarato <sirasatarato@gmail.com>
2020-10-19 22:12:16 +0900

    Change font to bold

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/Data/PostTags.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/Data/PostTags.kt
@@ -1,7 +1,9 @@
 package com.sinagram.yallyandroid.Home.Data
 
+import android.graphics.Typeface
 import android.text.Spannable
 import android.text.Spanned
+import android.text.style.StyleSpan
 import androidx.core.content.res.ResourcesCompat
 import com.sinagram.yallyandroid.R
 import com.sinagram.yallyandroid.Util.FontSpan
@@ -17,7 +19,7 @@ class PostTags(val content: String) {
             if (content[index] == ' ') isCharacter = false
             if (isCharacter) {
                 span.setSpan(
-                    FontSpan(ResourcesCompat.getFont(context!!, R.font.notosanskr_medium)),
+                    StyleSpan(Typeface.BOLD),
                     index,
                     index + 1,
                     Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
