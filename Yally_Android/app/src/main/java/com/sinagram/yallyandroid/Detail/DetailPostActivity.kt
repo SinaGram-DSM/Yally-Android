21707bdee9853c0e0ce945c52be832b927004210
sirasatarato <sirasatarato@gmail.com>
2020-10-21 20:07:44 +0900

    Create DetailPostActivity

---
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Detail/DetailPostActivity.kt
@@ -0,0 +1,12 @@
+package com.sinagram.yallyandroid.Detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sinagram.yallyandroid.R
+
+class DetailPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_post)
+    }
+}
