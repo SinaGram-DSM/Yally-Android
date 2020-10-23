6609b3304e7687b3ecae5d60e2ba3a923e26df85
sirasatarato <sirasatarato@gmail.com>
2020-10-23 18:40:23 +0900

    Create CommentRequest

---
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Detail/Data/CommentRequest.kt
@@ -0,0 +1,23 @@
+package com.sinagram.yallyandroid.Detail.Data
+
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
+
+data class CommentRequest(
+    var content: String,
+    var file: File? = null,
+    var requestHashMap: HashMap<String, RequestBody> = HashMap()
+) {
+    fun addComment() {
+        requestHashMap["content"] = content.toRequestBody("text/plain".toMediaTypeOrNull())
+    }
+
+    fun addFile() {
+        file?.let {
+            requestHashMap["file"] = file!!.asRequestBody("multipart/form-data".toMediaTypeOrNull())
+        }
+    }
+}
