4dfe993e42abe2e18dc15e7b56a1c7794deea984
sirasatarato <sirasatarato@gmail.com>
2020-10-28 09:36:29 +0900

    Delete variable StartTextView

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Util/ProgressBarThread.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Util/ProgressBarThread.kt
@@ -1,25 +1,18 @@
 package com.sinagram.yallyandroid.Util
 
-import android.annotation.SuppressLint
 import android.media.MediaPlayer
-import android.os.Handler
-import android.os.Message
 import android.util.Log
 import android.widget.SeekBar
-import android.widget.TextView
-import java.text.SimpleDateFormat
-import java.util.*
 
 class ProgressBarThread(
-    private val post_player_seekBar: SeekBar,
+    private val post_player_seekBar: SeekBar?,
     private val mediaPlayer: MediaPlayer?,
 ) : Thread() {
     override fun run() {
         try {
             if (mediaPlayer != null) {
                 while (!isInterrupted) {
-                    post_player_seekBar.progress = mediaPlayer.currentPosition
-//                    handler.sendEmptyMessage(0)
+                    post_player_seekBar?.progress = mediaPlayer.currentPosition
                 }
             }
         } catch (e: Exception) {
