b065d870a6fe7da9f8555f8175ca5fdea244bb14
sirasatarato <sirasatarato@gmail.com>
2020-10-28 09:35:59 +0900

    Make to renew posts

---
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/Data/CallPostsUp.kt
@@ -0,0 +1,19 @@
+package com.sinagram.yallyandroid.Home.Data
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sinagram.yallyandroid.Home.ViewModel.TimeLineViewModel
+
+class CallPostsUp(val data: List<Post>, val timeLineViewModel: TimeLineViewModel): RecyclerView.OnScrollListener() {
+    var pageId = 1
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+        LinearLayoutManager::class.java.cast(recyclerView.layoutManager)!!.run {
+            if (findLastCompletelyVisibleItemPosition() >= itemCount - 1 && data.isNotEmpty()) {
+                pageId++
+                timeLineViewModel.getTimeLineItem(pageId)
+            }
+        }
+    }
+}
