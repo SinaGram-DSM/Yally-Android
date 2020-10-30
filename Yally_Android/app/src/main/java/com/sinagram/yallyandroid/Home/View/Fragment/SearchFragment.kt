9419775d4c9feb4701497b9328cf6e985e6df416
sirasatarato <sirasatarato@gmail.com>
2020-10-30 11:21:15 +0900

    Make to change layout manager when click more text

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/View/Fragment/SearchFragment.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/View/Fragment/SearchFragment.kt
@@ -7,13 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.sinagram.yallyandroid.Home.View.FriendAdapter
 import com.sinagram.yallyandroid.Home.ViewModel.SearchViewModel
 import com.sinagram.yallyandroid.R
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
     private val searchViewModel: SearchViewModel by viewModels()
+    private var isExpand = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,13 +28,36 @@ class SearchFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        setClickMoreTextView()
         searchViewModel.getRecomendedList()
         searchViewModel.recomendListLivedata.observe(viewLifecycleOwner, {
             search_result_recyclerView.run {
                 setHasFixedSize(true)
-                layoutManager = LinearLayoutManager(context)
-
+                layoutManager = LinearLayoutManager(context).apply {
+                    orientation = LinearLayoutManager.HORIZONTAL
+                }
+                adapter = FriendAdapter(it)
             }
         })
+    }
+    
+    fun setClickMoreTextView() {
+        search_expand_textView.setOnClickListener {
+            if (isExpand) {
+                search_expand_textView.text = getString(R.string.briefly)
+                search_result_recyclerView.run {
+                    layoutManager = GridLayoutManager(context, 3)
+                }
+            } else {
+                search_expand_textView.text = getString(R.string.briefly)
+                search_result_recyclerView.run {
+                    layoutManager = LinearLayoutManager(context).apply {
+                        orientation = LinearLayoutManager.HORIZONTAL
+                    }
+                }
+            }
+
+            isExpand = !isExpand
+        }
     }
 }
