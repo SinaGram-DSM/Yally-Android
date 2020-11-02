e85d2bacaf87b169a1520dd3c90fe2d37407cbe8
minhee <jwmh15@gmail.com>
2020-11-02 23:03:04 +0900

    Modify Files

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Writing/ViewModel/WritingViewModel.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Writing/ViewModel/WritingViewModel.kt
@@ -1,19 +1,77 @@
 package com.sinagram.yallyandroid.Writing.ViewModel
 
+import android.media.MediaRecorder
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.sinagram.yallyandroid.Writing.Data.WritingRepository
 import com.sinagram.yallyandroid.Writing.Data.WritingRequest
+import java.lang.Exception
 
-class WritingViewModel:ViewModel(){
+
+class WritingViewModel : ViewModel() {
     private val repository = WritingRepository()
+    var mediaRecorder: MediaRecorder? = null
+    private var havefile = false
 
-    suspend fun writing(part: WritingRequest){
-        if(part!=null){
+    suspend fun writing(part: WritingRequest) {
+        if (part != null) {
             repository.writing(part)
+        } else {
+            Log.d("writingViewModel", "part null")
         }
-        else {
-            println("part==null")
+    }
+
+    suspend fun startRecording(filePath: String) {
+        try {
+            mediaRecorder = MediaRecorder()
+            mediaRecorder?.let {
+                it.setAudioSource(android.media.MediaRecorder.AudioSource.MIC)
+                it.setOutputFormat(android.media.MediaRecorder.OutputFormat.MPEG_4)
+                it.setAudioEncoder(android.media.MediaRecorder.AudioEncoder.DEFAULT)
+                it.setOutputFile(filePath)
+                it.prepare()
+                it.start()
+            }
+        }catch(e:Exception){
+            println(e.message)
         }
+
+    }
+
+    suspend fun stopRecording(){
+        mediaRecorder?.let{
+            it.stop()
+            it.release()
+            havefile = true
+        }
+        mediaRecorder = null
+    }
+
+    suspend fun hashtags(text: String) {
+        var copy = text
+        var result = ArrayList<String>()
+        var count = 0
+        var startIndex = 0
+
+        for (element in copy) {
+            if (element == '#')
+                count++
+        }
+
+        for (i in 0 until count) {
+            for (j in text) startIndex = text.indexOf('#') + 1
+
+            try {
+                result.add(copy.substring(startIndex, (copy.substring(startIndex).indexOf(" ") + startIndex)))
+                copy = copy.substring((copy.substring(startIndex).indexOf(" ") + startIndex) + 1)
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun sendData(writing: String){
+
     }
 
 }
--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Writing/WritingActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Writing/WritingActivity.kt
@@ -1,103 +1,152 @@
 package com.sinagram.yallyandroid.Writing
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.media.MediaRecorder
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.sinagram.yallyandroid.R
+import com.sinagram.yallyandroid.Splash.SplashActivity
 import kotlinx.android.synthetic.main.activity_writing.*
-import java.io.File
+
+private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
+private const val REQUEST_OPEN_GALLERY_PERMISSION = 300
 
 class WritingActivity : AppCompatActivity() {
-    private var mediaRecorder: MediaRecorder? = null
     private var count = 1
-    private var fileName: String = " "
     private val OPEN_GALLERY = 1
-
+    private val OPEN_AUDIO = 2
+    private var audioUri: Uri? = null
+    private var imageUri: Uri? = null
+    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writing)
 
-        //버튼 눌렀을 때 보여주기
-        writing_recording_button.setOnClickListener {
-            writing_recording_imageView.visibility = View.VISIBLE
-            writing_recording_button.visibility = View.VISIBLE
-            writing_text_textView.visibility = View.VISIBLE
+        setSupportActionBar(writing_toolbar)
+        writing_toolbar.setTitle(" ")
 
+        writing_recording_button.setOnClickListener {
             count++
             recording(count)
         }
 
-        //이미지 가져오기
         writing_voiceCover_button.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = MediaStore.Images.Media.CONTENT_TYPE
-            startActivityForResult(intent, OPEN_GALLERY)
+            getImage()
+        }
+
+        writing_voiceFile_button.setOnClickListener {
+            getVoice()
         }
 
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.toolbar_back_item -> {
+                changeView()
+                return true
+            }
+            R.id.toolbar_upload_item -> {
+                sendData()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
 
-    //이미지 세팅
+        }
+    }
+
+    private fun changeView(){
+        val intent = Intent(this@WritingActivity, HomeActivity::class.java)
+    }
+
+    private fun sendData() {
+        var writing = writing_writing_edit.toString()
+        changeView()
+    }
+
+    private fun getVoice() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = ("audio/*")
+        intent.type = MediaStore.Audio.Media.CONTENT_TYPE
+        startActivityForResult(intent, OPEN_AUDIO)
+    }
+
+    private fun getImage() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = MediaStore.Images.Media.CONTENT_TYPE
+            startActivityForResult(intent, OPEN_GALLERY)
+        } else {
+            ActivityCompat.requestPermissions(this@WritingActivity, permissions, REQUEST_OPEN_GALLERY_PERMISSION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else false
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                OPEN_AUDIO -> {
+                    if (data?.extras?.get("data") != null) {
+                        audioUri = data?.data
+                        writing_voice_imageView.setImageResource(R.drawable.icon_sound)
+                    }
+                }
+                OPEN_GALLERY -> {
+                    if (data?.extras?.get("data") != null) {
+                        imageUri = data?.data
+                        writing_voiceCover_imageView.setImageURI(imageUri)
+                    }
 
-            if (requestCode == OPEN_GALLERY) {
-                if (data?.extras?.get("data") != null) {
-                    val uri = data?.data
-                    writing_voiceCover_imageView.setImageURI(uri)
                 }
             }
-
         }
     }
 
     // 녹음
     private fun recording(count: Int) {
-        mediaRecorder = MediaRecorder()
-        mediaRecorder?.let {
-            if(count/2==0){
-                it.setAudioSource(MediaRecorder.AudioSource.MIC)
-                it.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-                it.setOutputFile(fileName)
-                it.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
 
+            writing_recording_imageView.visibility = View.VISIBLE
+            writing_recording_textView.visibility = View.VISIBLE
+            writing_text_textView.visibility = View.VISIBLE
 
-                try{
-                    it.prepare()
-                }catch (e: Exception) {
-                    Log.d(LOG_CAT,"prepare() failed")
+            when (count % 2) {
+                0 -> {
+
                 }
-                it.start()
-            } else {
-                it.stop()
-                it.release()
 
-                mediaRecorder = null
+                1 -> {
+                    writing_recording_imageView.visibility = View.GONE
+                    writing_recording_textView.visibility = View.GONE
+                    writing_text_textView.visibility = View.GONE
+                }
             }
-
-
-
-
-            if (count / 2 == 0) {
-                it.prepare()
-                it.start()
-            } else if (it != null) {
-                var file = File(fileName)
-                it.stop()
-                it.release()
-                println("success")
-
-//                var audioUri = contentResolver.insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, ContentValues?)
-            }
+        } else {
+            ActivityCompat.requestPermissions(this@WritingActivity, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
         }
+
     }
-
-
 }
