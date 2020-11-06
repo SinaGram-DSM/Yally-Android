49474cad760778ecff402a0a94d6a475b591427a
minhee <jwmh15@gmail.com>
2020-11-06 23:06:22 +0900

    Modify API

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Network/YallyAPI.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Network/YallyAPI.kt
@@ -1,6 +1,7 @@
 package com.sinagram.yallyandroid.Network
 
 import com.sinagram.yallyandroid.Writing.Data.WritingRequest
+import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -11,6 +12,6 @@ interface YallyAPI {
     @Multipart
     @POST("/post")
     suspend fun writing (
-        @Part part: WritingRequest
+        @Part part: HashMap<String,RequestBody>
     ): Response<Void>
 }
