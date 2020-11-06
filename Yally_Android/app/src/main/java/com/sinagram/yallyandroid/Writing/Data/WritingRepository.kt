0355bdf77265563e3b1aa3fb17564c445a0c613b
minhee <jwmh15@gmail.com>
2020-11-06 23:15:14 +0900

    Modify repository

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Writing/Data/WritingRepository.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Writing/Data/WritingRepository.kt
@@ -4,12 +4,11 @@ import com.sinagram.yallyandroid.Base.BaseRepository
 import com.sinagram.yallyandroid.Network.Result
 import com.sinagram.yallyandroid.Network.YallyConnector
 
-
 class WritingRepository : BaseRepository() {
 
     suspend fun writing(part: WritingRequest): Result<Void> {
         return mappingToResult {
-            YallyConnector.createAPI().writing(part)
+            YallyConnector.createAPI().writing()
         }
     }
 
