adcddace95058f30ef6eea5c513e8e7ba9f504ec
minhee <jwmh15@gmail.com>
2020-11-09 20:05:11 +0900

    Modify Network

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Network/YallyAPI.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Network/YallyAPI.kt
@@ -1,17 +1,14 @@
 package com.sinagram.yallyandroid.Network
 
-import com.sinagram.yallyandroid.Writing.Data.WritingRequest
 import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
-
+import retrofit2.http.*
 
 interface YallyAPI {
+    @Headers("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8")
     @Multipart
     @POST("/post")
     suspend fun writing (
-        @Part part: HashMap<String,RequestBody>
+        @PartMap part: HashMap<String,RequestBody>
     ): Response<Void>
 }
