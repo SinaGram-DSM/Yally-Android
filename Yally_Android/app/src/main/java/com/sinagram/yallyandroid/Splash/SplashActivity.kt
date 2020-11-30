bbe09d2b39ab5c4373b8343651dc65e0a3f325f2
sirasatarato <sirasatarato@gmail.com>
2020-11-30 15:27:57 +0900

    Fixed splash UI

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Splash/SplashActivity.kt
@@ -1,10 +1,10 @@
 package com.sinagram.yallyandroid.Splash
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import android.view.WindowManager
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.sinagram.yallyandroid.Home.View.HomeActivity
 import com.sinagram.yallyandroid.R
@@ -33,19 +33,22 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun isLogin() {
-        if (SharedPreferencesManager.getInstance().isLogin){
+        if (SharedPreferencesManager.getInstance().isLogin) {
             startActivity(Intent(this, HomeActivity::class.java))
             finish()
         }
     }
 
     private fun setTransparentStatusBar() {
-        window.apply {
+        with(window) {
             statusBarColor = Color.TRANSPARENT
-            setFlags(
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-            )
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                this.setDecorFitsSystemWindows(false)
+            } else {
+                this.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
         }
     }
 }
