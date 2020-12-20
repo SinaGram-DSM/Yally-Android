9ae152150fae1335aad26c8fec5cbdcdaa9ea04e
minhee <jwmh15@gmail.com>
2020-12-20 23:23:53 +0900

    Modify Post data

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Profile/Data/Post.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Profile/Data/Post.kt
@@ -1,19 +1,23 @@
 package com.sinagram.yallyandroid.Profile.Data
 
+data class Posts(
+	val posts: List<Post>
+)
+
 data class Post(
-	val postId: String,
+	val id: String?,
 	val user: profileUser,
 	val content: String,
-	val sound: String,
-	val img: String,
-	val comment: String,
-	val yally: String,
+	val sound: String?,
+	val img: String?,
+	val comment: Int,
+	val yally: Int,
 	val isYally: Boolean,
 	val createdAt: String
 )
 
 data class profileUser(
-		val email: String,
-		val nickname: String,
-		val img: String
+	val email: String?,
+	val nickname: String,
+	val img: String?
 )
