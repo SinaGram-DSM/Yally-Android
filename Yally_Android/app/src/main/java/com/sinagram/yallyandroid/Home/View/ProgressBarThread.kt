321958226aa606420a9baed41965a9343b4aebb0
minhee <jwmh15@gmail.com>
2020-12-20 23:19:38 +0900

    Add ProgressBarThread

---
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Home/View/ProgressBarThread.kt
@@ -0,0 +1,17 @@
+package com.sinagram.yallyandroid.Home.View
+
+import android.media.MediaPlayer
+import android.widget.SeekBar
+
+class ProgressBarThread(
+        private val post_player_seekBar: SeekBar,
+        private val mediaPlayer: MediaPlayer,
+        private val isPlaying: Boolean
+) : Thread() {
+
+    override fun run() {
+        while (isPlaying) {
+            post_player_seekBar.progress = mediaPlayer.currentPosition
+        }
+    }
+}
