63f579df32c26c7cd669143ae2a045493c896548
minhee <jwmh15@gmail.com>
2020-12-20 23:27:11 +0900

    Modify Null check

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Profile/Ui/Adapter/ListenAdapter.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Profile/Ui/Adapter/ListenAdapter.kt
@@ -11,12 +11,12 @@ import com.bumptech.glide.Glide
 import com.sinagram.yallyandroid.Profile.Data.ListenUser
 import com.sinagram.yallyandroid.R
 
-class ListenAdapter(private val listenList:ArrayList<ListenUser>): RecyclerView.Adapter<ListenAdapter.ViewHolder>() {
+class ListenAdapter(private val listenList:List<ListenUser>): RecyclerView.Adapter<ListenAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val profileImg = itemView?.findViewById<ImageView>(R.id.listenItem_profile_imageView)
-        val nicknameTxt = itemView?.findViewById<TextView>(R.id.listenItem_nickname_textView)
-        val listenBtn = itemView?.findViewById<Button>(R.id.listenItem_listening_button)
+        val profileImg = itemView.findViewById<ImageView>(R.id.listenItem_profile_imageView)
+        val nicknameTxt = itemView.findViewById<TextView>(R.id.listenItem_nickname_textView)
+        val listenBtn = itemView.findViewById<Button>(R.id.listenItem_listening_button)
 
         fun bind(listenUser: ListenUser){
             Glide.with(itemView).load(listenUser.image).into(profileImg)
@@ -36,7 +36,7 @@ class ListenAdapter(private val listenList:ArrayList<ListenUser>): RecyclerView.
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder?.bind(listenList[position])
+        holder.bind(listenList[position])
     }
 
     override fun getItemCount(): Int = listenList.size
