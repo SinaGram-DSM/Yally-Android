aa3351df8979ef02fc37f0e96e43d93c9a3a5725
minhee <jwmh15@gmail.com>
2020-12-20 23:21:22 +0900

    Add method

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Profile/ViewModel/ProfielViewModel.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Profile/ViewModel/ProfielViewModel.kt
@@ -1,19 +1,21 @@
 package com.sinagram.yallyandroid.Profile.ViewModel
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sinagram.yallyandroid.Profile.Data.ProfileRepository
 import kotlinx.coroutines.launch
 import com.sinagram.yallyandroid.Network.Result
-import com.sinagram.yallyandroid.Profile.Data.User
+import com.sinagram.yallyandroid.Profile.Data.*
 import okhttp3.MultipartBody
-import okhttp3.RequestBody
 
-class ProfileViewModel: ViewModel() {
+class ProfileViewModel : ViewModel() {
     private val repository = ProfileRepository()
+    val listenLiveData: MutableLiveData<ListenList> = MutableLiveData<ListenList>()
+    val setLiveData: MutableLiveData<User> = MutableLiveData()
+    val timeLineData: MutableLiveData<List<Post>> = MutableLiveData()
 
-    fun setProfile(){
+    fun setProfile() {
         viewModelScope.launch {
             val result = repository.setProfile()
 
@@ -26,38 +28,80 @@ class ProfileViewModel: ViewModel() {
     }
 
     private fun setProfileSuccess(result: Result.Success<User>){
-
-    }
-
-    fun modifyProfile(nickname: MultipartBody.Part, imagePart:MultipartBody.Part) {
-        viewModelScope.launch {
-            Log.e("profileViewModel", nickname.toString())
-
-            val result = repository.modifyProfile(nickname,imagePart)
-
-            if(result is Result.Success){
-                Log.e("profileViewModel","modifySuccess")
-            } else {
-                Log.e("profileViewModel",(result as Result.Error).exception)
+        if(result.code == 200){
+            val data = result.data
+            if(data != null){
+                setLiveData.postValue(data)
             }
         }
     }
 
-    fun getListening(){
+    fun getListening() {
         viewModelScope.launch {
             val result = repository.getListening()
+            if (result is Result.Success) {
+                setListenSuccess(result)
+            } else {
+                Log.e("profileViewModel", (result as Result.Error).exception)
+            }
         }
     }
 
-    fun getListener(){
-        viewModelScope.launch{
-            val result = repository.getListener()
-        }
-    }
-
-    fun getMyPost(){
+    fun getListener() {
         viewModelScope.launch {
-            val result = repository.getMyPost()
+            val result = repository.getListener()
+            if (result is Result.Success) {
+                setListenSuccess(result)
+                Log.e("profileViewModel","getListener")
+            } else {
+                Log.e("profileViewModel", (result as Result.Error).exception)
+            }
         }
     }
+
+    private fun setListenSuccess(result: Result.Success<ListenList>){
+        if(result.code == 200) {
+            val data = result.data
+            if(data != null){
+                listenLiveData.postValue(data)
+
+                Log.e("profileViewModel","listen liveData insert")
+            }
+        }
+    }
+
+    fun setMyTimeLine() {
+        viewModelScope.launch {
+            val result = repository.setMyTimeLine()
+            if(result is Result.Success){
+                setTimeLineSuccess(result)
+            }
+        }
+    }
+
+    private fun setTimeLineSuccess(result: Result.Success<Posts>){
+        if(result.code == 200) {
+            val data = result.data
+            if(data != null) {
+                timeLineData.postValue(data.posts)
+
+                Log.e("profileViewModel","timeLine liveData insert")
+            }
+        }
+    }
+
+    fun modifyProfile(nickname: MultipartBody.Part, imagePart: MultipartBody.Part) {
+        viewModelScope.launch {
+            Log.e("profileViewModel", nickname.toString())
+
+            val result = repository.modifyProfile(nickname, imagePart)
+
+            if (result is Result.Success) {
+                Log.e("profileViewModel", "modifySuccess")
+            } else {
+                Log.e("profileViewModel", (result as Result.Error).exception)
+            }
+        }
+    }
+
 }
