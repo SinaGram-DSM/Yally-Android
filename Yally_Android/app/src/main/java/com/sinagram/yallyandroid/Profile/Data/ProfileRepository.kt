182cfeeb34562b935bfb7c18d7b8ed9f88849336
minhee <jwmh15@gmail.com>
2021-01-04 22:38:04 +0900

    Modify BasePostRepository

--- Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Profile/Data/ProfileRepository.kt
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Profile/Data/ProfileRepository.kt
@@ -1,13 +1,14 @@
 package com.sinagram.yallyandroid.Profile.Data
 
 import android.util.Log
+import com.sinagram.yallyandroid.Base.BasePostRepository
 import com.sinagram.yallyandroid.Base.BaseRepository
 import com.sinagram.yallyandroid.Home.Data.Post
 import com.sinagram.yallyandroid.Network.Result
 import com.sinagram.yallyandroid.Network.YallyConnector
 import okhttp3.MultipartBody
 
-class ProfileRepository: BaseRepository() {
+class ProfileRepository: BasePostRepository() {
     val testToken:String = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8"
     val testEmail:String = "admin@gmail.com"
     var test: String = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8"
@@ -53,6 +54,5 @@ class ProfileRepository: BaseRepository() {
             YallyConnector.createAPI().modifyProfile(testToken, nickname,imagePart)
         }
     }
-
 
 }
