29fdf67dcb1c9a72498ca754da07e612534530d7
minhee <jwmh15@gmail.com>
2021-02-09 23:43:56 +0900

    Create MediaRecorder

---
+++ Yally_Android/app/src/main/java/com/sinagram/yallyandroid/Writing/MediaRecorder.kt
@@ -0,0 +1,33 @@
+package com.sinagram.yallyandroid.Writing
+
+import android.media.MediaRecorder
+import android.util.Log
+import java.io.IOException
+
+class MediaRecorder {
+    var recorder: MediaRecorder? = null
+
+    fun startRecorder(filepath: String){
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setOutputFile(filepath)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+
+            try{
+                prepare()
+            }catch (e: IOException){
+                Log.e("MediaRecorder","prepare() failed")
+            }
+        }
+    }
+
+    fun stopRecorder(){
+        recorder?.apply {
+            stop()
+            release()
+        }
+        recorder = null
+    }
+
+}
